57ed068f9fc991776caa189eb3345067e2ff5918
Liane Hampe <liaham@xmera.de>
2021-10-11 20:04:07 +0200

    Prepare for version 0.1.0

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_subproject_collapse do
   name 'Sub Projects Collapse Plugin'
   author 'Anton Sergeev'
   description 'This plugin provides ability to collapse sub projects'
-  version '0.0.1'
+  version '0.1.0'
   url 'https://github.com/anton-sergeev/redmine_subproject_collapse'
   author_url 'https://github.com/anton-sergeev'
 end
